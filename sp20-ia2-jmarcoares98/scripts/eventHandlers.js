e8043e182c8b5f1a23d891c6e94af23ddda939fd
Marco Ares <39780326+jmarcoares98@users.noreply.github.com>
2020-01-29 22:26:35 -0800

    Completed Most of Todos
    There were some to do lists in the comment block that I completed(mostly)

--- sp20-ia2-jmarcoares98/scripts/eventHandlers.js
+++ sp20-ia2-jmarcoares98/scripts/eventHandlers.js
@@ -9,25 +9,27 @@
   //menu bottom disabled, UI mode = login
   function startUp() {
     //Hide all pages except for Login Page, which is the start page.
-    document.getElementById("viewDataPageDiv").style.display = "none";
-    document.getElementById("addDataPageDiv").style.display = "none";
+    document.getElementById("displayDataModeDiv").style.display = "none";
+    document.getElementById("addDataModeDiv").style.display = "none";
+
+    document.getElementById("loginModeDiv").style.display = "block";
     //Clear all text from email and password fields
     //TO DO: Fill in
 
     //Set top bar text
-    //TO DO: Fill in
+    document.getElementById("topBarTitle").textContent = "welcome to my app";
 
     //Hide the bottom bar initially
-    //TO DO: Fill in
+    document.getElementById("bottomBar").style.visibility = "hidden";
 
     //Hide all menu items except for items of current mode:
     //TO DO: Fill in
 
     //Disable menu button:
-    //TO DO: Fill in
+    document.getElementById("menuBtn").disabled = true;
 
     //Set current mode
-    //TO DO: Fill in
+    mode = "loginMode";
 
     //set the input focus to the email field
     //TO DO: Fill in
@@ -61,32 +63,56 @@ document.getElementById("menuBtn").addEventListener("click",function(e) {
 //bottomBarBtnClick -- When a button in the bottom bar is clicked, we potentially
 //need to toggle the mode.
 var bottomBarBtnClick = function() {
-  //TO DO: Fill in
+  if (mode != this.id) {
+    //this changes the mode buttons
+    document.getElementById(mode).classList.remove("menuItemSelected");
+    document.getElementById(mode + "Div").style.display = "none";
+    document.getElementById(mode + "Menu").style.display = "none";
+    this.classList.add("menuItemSelected");
+    let menuItems = document.getElementsByClassName(mode + "Item");
+    //not displaying the other modes or changing pages
+    for (let i = 0; i < menuItems.length; ++i) {
+      menuItems[i].style.display = "none";
+    }
+    //this is where the changes happen when switching modes to current mode
+    mode = this.id;
+    document.getElementById("topBarTitle").textContent = modeToTitle[mode];
+    document.getElementById(mode + "Div").style.display = "block";
+    document.getElementById(mode + "Menu").style.display = "block";
+    menuItems = document.getElementsByClassName(mode + "Item");
+    for (let i = 0; i < menuItems.length; ++i) {
+      menuItems[i].style.display = "block";
+    }
+  }
 }
 
 //login -- This function sets the initial app state after login. It is called
 //from setTimeout after the button spinner has commenced.bottombar
 function login() {
   //Stop spinner
- //TO DO: Fill in
+  document.getElementById("loginBtnIcon").classList.remove("fas","fa-spinner","fa-spin");
   
   //Enable menu button:
-  //TO DO: Fill in
+  document.getElementById("menuBtn").disabled = false;
 
   //Show bottom bar buttons and highlight feed mode button
-  //TO DO: Fill in
+  document.getElementById("bottomBar").style.visibility = "visible";
+  document.getElementById("feedMode").classList.add("menuItemSelected");
+  document.getElementById("roundsMode").classList.remove("menuItemSelected");
+  document.getElementById("coursesMode").classList.remove("menuItemSelected");
   
   //Change title bar to that of app start page
-  //TO DO: Fill in
+  document.getElementById("topBarTitle").textContent = modeToTitle[mode];
  
   //Show only the menu items for current mode
   //TO DO: Fill in
 
   //hide login screen and show feed screen
-  //TO DO: Fill in
+  document.getElementById("loginModeDiv").style.display = "none";
+  document.getElementById(mode + "MainDiv").style.display = "block";
 
   //Set mode to current mode
-  //TO DO: Fill in
+  mode = "displayDataMode";
 }
 
 //loginInterface submit: When the login button is clicked, we rely on form
